ff8ac9a803652288efe7446e04f5edb4a6c49c80
David Antillies <93983691+dongyan3721@users.noreply.github.com>
2024-03-23 10:01:46 +0800

    菜品识别框样式调整完毕

--- src/components/echart-dom/DishRecognization.vue
+++ src/components/echart-dom/DishRecognization.vue
@@ -59,7 +59,7 @@ let cropOptions = reactive({
   autoCrop: true, // 是否默认生成截图框
   autoCropWidth: 150, // 默认生成截图框宽度
   autoCropHeight: 150, // 默认生成截图框高度
-  fixedBox: false, // 固定截图框大小 不允许改变
+  fixedBox: true, // 固定截图框大小 不允许改变
   fixed: true, // 是否开启截图框宽高固定比例，这个如果设置为true，截图框会是固定比例缩放的，如果设置为false，则截图框的狂宽高比例就不固定了
   fixedNumber: [1, 1], // 截图框的宽高比例 [ 宽度 , 高度 ]
   canMove: true, // 上传图片是否可以移动
@@ -165,85 +165,136 @@ let dish = reactive({
 
 <template>
   <div id="dish-wrapper">
-    <div id="dish-recognize">
-      <!--主页面内图片预览对话框-->
-      <el-dialog v-model="bodyPreviewDialogVis" @close="handleBodyPreviewDialogClose" :width="bodyPreviewDialogWidth" append-to-body>
-        <img :src="dialogUploadFileList[0].url" alt="preview" :height="dialogImageHeight" :width="dialogImageWidth"/>
-        <template #header><span style="color: #666666">图片预览</span></template>
-      </el-dialog>
-      <el-dialog v-model="cropVis" @close="handleCloseCropDialog" width="600px" append-to-body>
-        <template #header>
-          <!--      <template #header style="display: flex; align-items: center; justify-content: flex-start;">-->
-          <el-icon color="#666666" size="large">
-            <Scissor/>
-          </el-icon>
-          <span style="color: #666666">图片上传裁剪</span>
-        </template>
-        <el-row>
-          <VueCropper style="min-width: 560px; min-height: 300px" ref="cropperRef" :img="cropOptions.img" :outputSize="cropOptions.outputSize"
-                      :outputType="cropOptions.outputType" :info="cropOptions.info" :canScale="cropOptions.canScale" :autoCrop="cropOptions.autoCrop"
-                      :autoCropWidth="cropOptions.autoCropWidth" :autoCropHeight="cropOptions.autoCropHeight" :fixedBox="cropOptions.fixedBox"
-                      :fixed="cropOptions.fixed" :fixedNumber="cropOptions.fixedNumber" :canMove="cropOptions.canMove" :canMoveBox="cropOptions.canMoveBox"
-                      :original="cropOptions.original" :centerBox="cropOptions.centerBox" :infoTrue="cropOptions.infoTrue" :full="cropOptions.full"
-                      :enlarge="cropOptions.enlarge" :mode="cropOptions.mode" :key="cnt">
-          </VueCropper>
-        </el-row>
-        <el-row style="width: 100%; height: 60px; margin-top: 10px;">
-          <el-button type="primary" plain @click="rotateLeft">←向左旋转图片</el-button>
-          <el-button type="primary" plain @click="rotateRight">向右旋转图片→</el-button>
-          <el-button type="primary" @click="getCropDataBase64">获取截取的图片</el-button>
-        </el-row>
-        <hr>
-        <div>照片预览</div>
-        <div style="width: 600px; height: auto; display: flex; align-items: center; justify-content: center">
-          <!-- 若图片只设置宽度，可以保持等比例展示图片 -->
-          <img :src="previewUrl" style="width: 50px; height: 50px" alt="preview" v-show="showPreview">
-          <div v-show="!showPreview">
-            <img src="@/assets/observe.png" alt="observe" height="44" width="88"/>
-            <span>还没有选择图片哦</span>
-          </div>
+    <el-dialog v-model="bodyPreviewDialogVis" @close="handleBodyPreviewDialogClose" :width="bodyPreviewDialogWidth" append-to-body>
+      <img :src="dialogUploadFileList[0].url" alt="preview" :height="dialogImageHeight" :width="dialogImageWidth"/>
+      <template #header><span style="color: #666666">图片预览</span></template>
+    </el-dialog>
+    <el-dialog v-model="cropVis" @close="handleCloseCropDialog" width="600px" append-to-body>
+      <template #header>
+        <!--      <template #header style="display: flex; align-items: center; justify-content: flex-start;">-->
+        <el-icon color="#666666" size="large">
+          <Scissor/>
+        </el-icon>
+        <span style="color: #666666">图片上传裁剪</span>
+      </template>
+      <el-row>
+        <VueCropper style="min-width: 560px; min-height: 300px" ref="cropperRef" :img="cropOptions.img" :outputSize="cropOptions.outputSize"
+                    :outputType="cropOptions.outputType" :info="cropOptions.info" :canScale="cropOptions.canScale" :autoCrop="cropOptions.autoCrop"
+                    :autoCropWidth="cropOptions.autoCropWidth" :autoCropHeight="cropOptions.autoCropHeight" :fixedBox="cropOptions.fixedBox"
+                    :fixed="cropOptions.fixed" :fixedNumber="cropOptions.fixedNumber" :canMove="cropOptions.canMove" :canMoveBox="cropOptions.canMoveBox"
+                    :original="cropOptions.original" :centerBox="cropOptions.centerBox" :infoTrue="cropOptions.infoTrue" :full="cropOptions.full"
+                    :enlarge="cropOptions.enlarge" :mode="cropOptions.mode" :key="cnt">
+        </VueCropper>
+      </el-row>
+      <el-row style="width: 100%; height: 60px; margin-top: 10px;">
+        <el-button type="primary" plain @click="rotateLeft">←向左旋转图片</el-button>
+        <el-button type="primary" plain @click="rotateRight">向右旋转图片→</el-button>
+        <el-button type="primary" @click="getCropDataBase64">获取截取的图片</el-button>
+      </el-row>
+      <hr>
+      <div>照片预览</div>
+      <div style="width: 600px; height: auto; display: flex; align-items: center; justify-content: center">
+        <!-- 若图片只设置宽度，可以保持等比例展示图片 -->
+        <img :src="previewUrl" style="width: 50px; height: 50px" alt="preview" v-show="showPreview">
+        <div v-show="!showPreview">
+          <img src="@/assets/observe.png" alt="observe" height="44" width="88"/>
+          <span>还没有选择图片哦</span>
         </div>
-        <template #footer>
-          <el-button type="primary" @click="transferUploadedToBuffer" :disabled="!showPreview">确定</el-button>
-        </template>
-      </el-dialog>
-      <el-upload
-          v-model:file-list="dialogUploadFileList"
-          ref="bodyUploadRef"
-          list-type="picture-card"
-          :on-preview="handlePictureCardPreview"
-          :on-exceed="handleExceed"
-          :on-remove="handleAvatarRemove"
-          :on-change="handleAvatarInitialUpload"
-          :before-remove="handleRejectRemove"
-          :auto-upload="false"
-          :limit="1">
-        <template #default>
-          <el-icon><Plus/></el-icon>
-        </template>
-      </el-upload>
+      </div>
+      <template #footer>
+        <el-button type="primary" @click="transferUploadedToBuffer" :disabled="!showPreview">确定</el-button>
+      </template>
+    </el-dialog>
+    <div id="dish-portrait-wrapper">
+      <div id="dish-portrait-wrapper-blur">
+        <div id="dish-recognize">
+          <!--主页面内图片预览对话框-->
+          <el-upload
+              v-model:file-list="dialogUploadFileList"
+              ref="bodyUploadRef"
+              list-type="picture-card"
+              :on-preview="handlePictureCardPreview"
+              :on-exceed="handleExceed"
+              :on-remove="handleAvatarRemove"
+              :on-change="handleAvatarInitialUpload"
+              :before-remove="handleRejectRemove"
+              :auto-upload="false"
+              :limit="1">
+            <template #default>
+              <el-icon><Plus/></el-icon>
+            </template>
+          </el-upload>
+        </div>
+      </div>
     </div>
     <div id="dish-recognize-info">
-      <div>菜品名称：{{dish.dishName}}</div>
-      <div>热量计数：{{dish.calorie}}</div>
-      <div>营养分析：{{dish.nutrient}}</div>
-      <div>优化建议：{{dish.advice}}</div>
+      <div class="text-result">菜品名称：{{dish.dishName}}</div>
+      <div class="text-result">热量计数：{{dish.calorie}}</div>
+      <div class="text-result">营养分析：{{dish.nutrient}}</div>
+      <div class="text-result">优化建议：{{dish.advice}}</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-:deep(.el-dialog__header) {
+:deep(.el-dialog .el-dialog__header) {
   display: flex;
   justify-content: flex-start;
   align-items: center;
 }
 #dish-wrapper{
   display: flex;
+  width: 800px;
+  height: 400px;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.6);
+  filter: none;
 }
 #dish-wrapper #dish-recognize-info{
   display: flex;
-  padding: 0 0 0 20px;
+  padding: 0 0 0 40px;
   flex-direction: column;
+  height: 300px;
+  width: 300px;
+  overflow: auto;
+}
+:deep(.el-upload--picture-card){
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 200px;
+  width: 200px;
+}
+:deep(.el-upload-list__item){
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 200px;
+  width: 200px;
+}
+#dish-portrait-wrapper-blur{
+  backdrop-filter: blur(10px);
+  background-color: rgba(255, 255, 255, 0.1);
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+}
+#dish-portrait-wrapper{
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url("src/assets/chicken.jpeg");
+  background-size: cover;
+  border-radius: 10px;
+}
+.text-result{
+  font-family: Consolas, Monaco, Droid, Sans, Mono, Source, Code, Pro, Menlo, Lucida, Sans, Type, Writer, Ubuntu, Mono;
+  font-size: 18px;
+  background-image: linear-gradient(to right, #7ed2e1, #c39bed, #d8d18e); /* 定义渐变颜色 */
+  -webkit-background-clip: text; /* Safari/Chrome 下的属性 */
+  background-clip: text; /* 将背景裁剪到文字形状上 */
+  color: transparent; /* 让文字透明，以便显示背景渐变 */
 }
 </style>
